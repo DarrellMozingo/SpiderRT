3a78b3d80677415b480a0b7555f83b604a3adea3
Darrell Mozingo <darrell.mozingo@7digital.com>
2012-03-30 12:49:47 +0100

    Formatting

--- SpiderRT.Web/Controllers/HomeController.cs
+++ SpiderRT.Web/Controllers/HomeController.cs
@@ -9,10 +9,10 @@ namespace SpiderRT.Web.Controllers
 {
 	public class HomeController : Controller
 	{
-		 public ActionResult Index()
-		 {
-		 	return View();
-		 }
+		public ActionResult Index()
+		{
+			return View();
+		}
 
 		[HttpPost]
 		public ActionResult Search(SearchViewModel viewModel)
