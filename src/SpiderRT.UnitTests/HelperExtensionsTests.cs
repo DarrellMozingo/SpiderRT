30ff452b01e4d96a43c0f6085ed53295892e891a
Darrell Mozingo <darrell@mozingo.net>
2012-07-16 15:02:23 +0100

    Added a unit test for the ForEach extension.

---
+++ src/SpiderRT.UnitTests/HelperExtensionsTests.cs
@@ -0,0 +1,18 @@
+﻿using System.Linq;
+using NUnit.Framework;
+
+namespace SpiderRT.UnitTests
+{
+	public class HelperExtensionsTests
+	{
+		[Test]
+		public void Should_loop_over_each_item_of_the_enumeration_executing_the_given_action()
+		{
+			var total = 0;
+
+			Enumerable.Range(1, 2).ForEach(x => total += x);
+
+			Assert.That(total, Is.EqualTo(3));
+		}
+	}
+}
